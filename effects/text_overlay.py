bcca0baa053b48b96c5e63f731f920dc47c9b1e6
Alexhong1269 <alexhong1269@gmail.com>
2026-08-31 16:21:11 -0400

    forgot to add the iteration through the lines

--- effects/text_overlay.py
+++ effects/text_overlay.py
@@ -14,6 +14,7 @@ class Banner:
 
         line_widths = [
             cv2.getTextSize(line, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness)[0][0]
+            for line in self.lines
         ]
 
         self.x = -max(line_widths)
